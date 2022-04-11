e85ba19f25b54d34ae50e174102a23984904ad6b
Rekayudha13 <yudhareka0@gmail.com>
2022-04-12 06:08:14 +0800

    reinstall laravel dan pemasangan template boostrap

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.head')
+
+@section('container')
+    <h1>ini blog</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.head')
+
+@section('container')
+  <h1>Oke</h1>    
+@endsection
